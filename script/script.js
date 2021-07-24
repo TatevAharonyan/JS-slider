4f4ac30dd4611dcbf27ea590cd641cf262589c4d
TatevAharonyan <tatevaharonyan1@gmail.com>
2021-07-24 18:56:11 +0400

    all HTML construction was changed  JS

--- script/script.js
+++ script/script.js
@@ -1,21 +1,113 @@
 
-let imgStyle = document.getElementById("img_carusel");
 let info = document.getElementsByClassName("info");
 let left_animation_div = document.getElementsByClassName("left_animation_div");
 let mini_img = document.getElementsByClassName("mini_img");
 let solid_line = document.getElementsByClassName("solid_line");
 let empty_circle_border = document.getElementsByClassName("empty_circle_div");
 let circle_div_color = document.getElementsByClassName("circle_div");
-let containerClass =  document.getElementsByClassName("container");
+let containerClass = document.getElementsByClassName("container");
+const imgSrcArr = ["../img/Insights_and_Reports.png", "../img/Manage_Leads_and_Deals.png", "../img/Track_Communications.png", "../img/Automate_and_Grow.png"];
+const imgAltArr = [" img Insights and Reports", " img Manage Leads and Deals", " img Track Communications", "img Automate and Grow"]
+const H3InfoArr = ["Insights and Reports", "Manage Leads and Deals", "Track Communications", "Automate and Grow"]
+const PInfoArr = ["Insights and Reports: Deep dive into metrics customized for your business and measure goals.",
+    "Get more hot leads fed straight into your sales pipelines around the clock.",
+    "Track Communications: Track calls, emails and contact history exactly where you need.",
+    "Automate and Grow: Eliminate busywork by automating repetitive administrative tasks."
+]
+const H1InnerText = "More sales. Less legwork."
+const PInnerText = "Get more leads, sort sales in one place and eliminate admin, because your day belongs to you."
+
 
 let sliderTimeout;
 let nextSlider = true;
 
+function createTextTag(tag, append, classIndividual = null ,idIndividual = null,  textInfo = null) {
+    let textTagUniversal = document.createElement(`${tag}`);
+    classIndividual !== null && (textTagUniversal.className = classIndividual);
+    idIndividual !== null && (textTagUniversal.id = idIndividual);
+    textInfo !== null && (textTagUniversal.innerText = textInfo);
+    return append.appendChild(textTagUniversal);
+}
 
-for (i = 0; i < 4; i++) {
+function createDiv(classIndividual, append, idIndividual = null) {
+    let divUniversal = document.createElement("div");
+     classIndividual !== null && (divUniversal.className = classIndividual);
+     idIndividual !== null && (divUniversal.id = idIndividual);
+    return append.appendChild(divUniversal)
+}
+
+function createImg(append, i, classIndividual) {
+    let imgUniversal = document.createElement("img");
+    imgUniversal.src = imgSrcArr[i];
+    imgUniversal.alt = imgAltArr[i];
+    imgUniversal.className = classIndividual;
+    return append.appendChild(imgUniversal)
+}
+
+function createA(append, i) {
+    let aUniversal = document.createElement("a");
+    aUniversal.href = imgSrcArr[i];
+    createImg(aUniversal, i, "img_slider")
+    return append.appendChild(aUniversal)
+}
+
+// HTML content
+
+let divIdWrapper = createDiv(null,  document.body, "wrapper");
+let divClassWrapper = createDiv("wrapper", divIdWrapper);
+// create title
+let divWrapperTitle = createDiv("wrapper_title", divClassWrapper);
+ createTextTag( "h1", divWrapperTitle , null, null, H1InnerText);
+createTextTag( "p", divWrapperTitle, null ,null, PInnerText);
+
+let divWrapperCarusel = createDiv("wrapper_carusel", divClassWrapper);
+
+// wrapper  button text img
+let divWrapperButtonTextImg = createDiv("wrapper_button_text_img", divWrapperCarusel);
+
+//  sliders 
+
+//  Photo section, which changes when you click on the left texts.
+let DivImgCaruselBackground = createDiv("img_carusel-background", divWrapperCarusel);
+let divImgContainerMiniImg = createDiv("img_container_mini_img", DivImgCaruselBackground);
+
+// mini img
+
+// add img_container class, elements "a" will be added to the directory
+let divImgContainer = createDiv("img_container", DivImgCaruselBackground);
+let divIdImgCarusel =  createDiv(null, divImgContainer, "img_carusel");
+
+// Background img and "a" element
+
+for (i = 0; i < imgSrcArr.length; i++) {
+
+    // sliders
+        let divContainerAniationDivTextInfo = createDiv("container_aniation_div_text_info", divWrapperButtonTextImg);
+        let divContainerAniation = createDiv("container_aniation", divContainerAniationDivTextInfo);
+        createDiv("background_dashed_line", divContainerAniation);
+        createDiv("solid_line", divContainerAniation);
+        var emptyCircleDiv = createDiv("empty_circle_div", divContainerAniation);
+        createDiv("circle_div", emptyCircleDiv);
+        let divContainer = createDiv("container", divContainerAniationDivTextInfo);
+        divContainer.addEventListener('mousedown', hendelClick);
+         createTextTag( "h3", divContainer, null ,`slider${[i]}`,H3InfoArr[i]);
+         createTextTag( "p", divContainer, "info" , null, PInfoArr[i]);
+    
+       
+        createImg(divContainer, i)
+
+    // mini img
+    var divMiniImg = createDiv("mini_img", divImgContainerMiniImg);
+    createDiv("left_animation_div", divMiniImg);
+    createTextTag( "span", divMiniImg, "mini_img_number", null, [i + 1]);
+    createImg(divMiniImg, i)
+
+    // Background img and "a" element
+    createA(divIdImgCarusel, i);
+
+    // add a click event listener to the class container
     containerClass[i].addEventListener('click', onClickSlider);
-};
-
+}
 
 // stop auto carusel
 function hendelClick() {
@@ -23,6 +115,7 @@ function hendelClick() {
     clearTimeout(sliderTimeout);
 }
 
+// the function will be called one time during file loading
 function loading() {
     if (window.innerWidth < 992) {
         removeEvent()
@@ -31,13 +124,13 @@ function loading() {
     }
 }
 
-// The function is called when the screen is resized. Depending on the size, add or remove sabitias.
+// The function is called when the screen is resized. Depending on the size, add or remove events.
 window.onresize = function (e) {
     if (e.target.window.innerWidth <= 992) {
         removeEvent()
     }
     else {
-        for (i = 0; i < 4; i++) {
+        for (i = 0; i < imgSrcArr.length; i++) {
             containerClass[i].addEventListener('click', onClickSlider);
         };
     }
@@ -46,7 +139,7 @@ window.onresize = function (e) {
 // During the click, a function is called, using the id it is decided which of the sliders should be active.
 function onClickSlider(e, sec = .2) {
     let id = e.target.id;
-    for (let i = 0; i < 4; i++) {
+    for (let i = 0; i < imgSrcArr.length; i++) {
         if (("slider" + `${i}`) === id) {
             let index = i;
             choiceOfStyles(sec, index)
@@ -55,18 +148,18 @@ function onClickSlider(e, sec = .2) {
 }
 // During the call of the function, all event Click is removed.
 function removeEvent() {
-    for (i = 0; i < 4; i++) {
+    for (i = 0; i < imgSrcArr.length; i++) {
         containerClass[i].removeEventListener('click', onClickSlider);
         info[i].style.display = "block";
         containerClass[i].style.backgroundColor = " #eceefa";
     };
 }
 
-// The function is called 1 time when the window is loaded and stops progress on the Click event.
+// The function decides which slide should be next. Thanks to the carousel stops function.
 function loadingSlider(sec, index) {
     let secondTimeout;
     choiceOfStyles(sec, index)
-    if (index + 1 === 0 || index + 1 > 4) {
+    if (index + 1 === 0 || index + 1 > imgSrcArr.length) {
         index = -1;
         secondTimeout = 0;
     } else {
@@ -75,13 +168,12 @@ function loadingSlider(sec, index) {
     if (nextSlider) { sliderTimeout = setTimeout(() => loadingSlider(3, index + 1), secondTimeout); }
 }
 
-
 // index dependency picks styles
 function choiceOfStyles(sec, index) {
-    for (let i = 0; i < 4; i++) {
+    for (let i = 0; i < imgSrcArr.length; i++) {
         if (i === index) {
-            imgStyle.style.marginLeft = `-${i * 40}vw`;
-            imgStyle.style.width = `${(i + 1) * 40}vw`;
+            divIdImgCarusel.style.marginLeft = `-${i * 40}vw`;
+            divIdImgCarusel.style.width = `${(i + 1) * 40}vw`;
             info[i].style.display = "block";
             solid_line[i].style.display = "block";
             solid_line[i].style.animationDuration = `${sec}s`;
@@ -110,3 +202,7 @@ function choiceOfStyles(sec, index) {
         }
     }
 }
+
+
+
+
